bc63c176f69d917a4236a4922ea39e01948f353f
Huỳnh Dũng <@huynhdungfed>
2021-07-29 18:18:38 +0700

    fix result - chưa add theme với trạng thái rolling

--- src/diceVer2/Dice_Ver2.js
+++ src/diceVer2/Dice_Ver2.js
@@ -1,45 +1,56 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 
 import "./Dice.css";
 function Dice_Ver2() {
   const [numberType, setNumberType] = useState(0);
   const [amountDice, setAmountDice] = useState(0);
   const [arrayResult, setArrayResult] = useState([]);
-  const [roll, setRoll] = useState("ROLL")
+  
 
   const sideDice = useRef(0);
   const numberDice = useRef(0);
   const resultDice = useRef(0);
   const sumDice = useRef(0)
 
-  let result = 0;
+  let result;
   let i;
 
+
   const submit = (e) => {
+    e.preventDefault();
     setNumberType(sideDice.current.value);
     setAmountDice(numberDice.current.value);
-    e.preventDefault();
+    
   };
 
   function createArrayOfDice() {
-    for (i = 1; i <= amountDice; i++) {
+      result = 0
+    for ( i = 1; i <= amountDice; i++) {
       result = Math.floor(Math.random() * numberType + 1);
       setArrayResult(arrayResult.push(result));
     }
   }
-  function sumOfAllDices() {
+
+
+  const sumOfAllDices = () => {
     resultDice.current.innerHTML = "";
+    result = 0
     for (i = 0; i < arrayResult.length; i++) {
-      result = result + arrayResult[i];
-      resultDice.current.innerHTML = `${resultDice.current.innerHTML}<div class='dice__roll'>${arrayResult[i]}</div>`;
+     console.log(`Đang check ở tổng tất cả dice - 1: ${result}`);
+      result += arrayResult[i];
+      console.log(`Đang check ở tổng tất cả dice - 2: ${result}`);
+     resultDice.current.innerHTML = `${resultDice.current.innerHTML}<div class='dice__roll'> ${arrayResult[i]} </div>`;
     }
     sumDice.current.innerHTML = `<div class='dicesum'>${result}</div>`;
+    console.log("------------------------------");
+
   }
 
   const handleRoll = () => {
     createArrayOfDice();
     sumOfAllDices();
     setArrayResult([]);
+ 
   };
 
   return (
@@ -60,10 +71,9 @@ function Dice_Ver2() {
          </div>
           <br />
           <input
-           
-            value={roll}
+            value="ROLL"
             type="submit"
-            className="dice__button theme-custom-1"
+            className="dice__button theme-custom-1 "
             onClick={handleRoll}
           />
         </form>
