1eb6e8f349815deaa6b72a92fa04d785f79dbf68
Huỳnh Dũng Hồ <hohuynhdung@gmail.com>
2021-08-02 16:02:06 +0700

    hello

--- src/diceVer2/Dice_Ver2.js
+++ src/diceVer2/Dice_Ver2.js
@@ -10,16 +10,26 @@ function Dice_Ver2() {
     D12: 12,
     D20: 20,
   };
+  const amount = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+  };
 
   const [numberType, setNumberType] = useState(typeDice.D4);
-  const [amountDice, setAmountDice] = useState(0);
+  const [amountDice, setAmountDice] = useState(amount[1]);
   const [arrayResult, setArrayResult] = useState([]);
-  const [amountRoll, setAmoutRoll] = useState(0)
+  const [amountRoll, setAmoutRoll] = useState(0);
 
   // const sideDice = useRef(0);
-  const numberDice = useRef(0);
+  // const numberDice = useRef(0);
   const resultDice = useRef(0);
-  const sumDice = useRef(0);
+  // const sumDice = useRef(0);
 
   let result;
   let i;
@@ -43,19 +53,28 @@ function Dice_Ver2() {
       result += arrayResult[i];
       resultDice.current.innerHTML = `${resultDice.current.innerHTML}<div class='dice__roll'> ${arrayResult[i]} </div>`;
     }
-    sumDice.current.innerHTML = `<div class='dicesum'>${result}</div>`;
+    // sumDice.current.innerHTML = `<div class='dicesum'>${result}</div>`;
   };
   const updateAmountRoll = () => {
-    if(amountDice == 0){setAmoutRoll(amountRoll)}
-    else {setAmoutRoll(amountRoll+1)}
-  }
- const resetRoll = ()=>{
-   setAmoutRoll(0);
- }
+    // eslint-disable-next-line
+    if (amountDice == 0) {
+      setAmoutRoll(amountRoll);
+    } else {
+      setAmoutRoll(amountRoll + 1);
+    }
+  };
+  const resetRoll = () => {
+    setAmoutRoll(0);
+    setNumberType(typeDice.D4);
+    setAmountDice(amount[1]);
+    setArrayResult([]);
+    sumOfAllDices()
+  
+  };
 
   const handleRoll = () => {
     createArrayOfDice();
-    updateAmountRoll()
+    updateAmountRoll();
     sumOfAllDices();
     setArrayResult([]);
   };
@@ -85,16 +104,29 @@ function Dice_Ver2() {
             <br />
             <div className="">
               <p className="dice-title dice-content">Nhập số xúc xắc</p>
-              <input
+              <select
+                className="dice-form"
+                value={amountDice}
+                onChange={(e) => setAmountDice(e.target.value)}
+              >
+                {Object.entries(amount).map((type) => (
+                  <option className="dice-option" value={type[1]}>
+                    {type[0]}
+                  </option>
+                ))}
+              </select>
+              {/* <input
                 onChange={(e) => setAmountDice(e.target.value)}
                 ref={numberDice}
                 type="text"
                 className="dice-form"
-              />
+              /> */}
             </div>
             <br />
             <div>
-            <p className="dice-title dice-content">Số lần bạn đã roll: {amountRoll}</p>
+              <p className="dice-title dice-content">
+                Số lần bạn đã roll: {amountRoll}
+              </p>
             </div>
             <input
               value="ROLL"
@@ -102,7 +134,7 @@ function Dice_Ver2() {
               className="dice-form dice-button"
               onClick={handleRoll}
             />
-               <input
+            <input
               value="RESET"
               type="submit"
               className="dice-form dice-button"
@@ -110,7 +142,7 @@ function Dice_Ver2() {
             />
           </form>
           <p ref={resultDice} className="dice-result" />
-          <p ref={sumDice} />
+          {/* <p ref={sumDice} /> */}
         </div>
         <div className="drops">
           <div className="drop drop-1" />
